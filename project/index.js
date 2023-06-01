b8356ea57c6ebd05c61d97649b5076314b9566f9
Alex Mootz <alexmootz@hotmail.com>
2023-06-01 09:40:57 -0400

    include rebuilding example

--- project/index.js
+++ project/index.js
@@ -4,3 +4,12 @@ import { buildScVal, xdr as DepXdr } from "xdr-dependency";
 let scVal = buildScVal();
 console.log("local xdr validation: ", xdr.ScVal.isValid(scVal));
 console.log("dep xdr validation: ", DepXdr.ScVal.isValid(scVal));
+
+// rebuilding the xdr with the local xdr version will fix validation
+let scValString = scVal.toXDR().toString("base64");
+let rebuiltScVal = xdr.ScVal.fromXDR(scValString, "base64");
+console.log("local xdr rebuild validation: ", xdr.ScVal.isValid(rebuiltScVal));
+console.log(
+  "rebuild is the same? ",
+  scValString == rebuiltScVal.toXDR().toString("base64")
+);
